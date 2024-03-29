209ae3411b8ad2e1f144b4843582beecc9f5c838
craizytech <craizytech@gmail.com>
2024-03-29 22:29:42 +0300

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	const binary_tree_t *parent = tree;
-	size_t left_height = 0, right_height = 0;
+	size_t r, l, height = 0;
 
 	if (tree == NULL)
-		return 0;
+		return -1;
+	r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	height += (r > l ? r : l);
 
-	left_height = binary_tree_height(parent->left);
-	right_height = binary_tree_height(parent->right);
-	if (left_height >= right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (height);
 }
